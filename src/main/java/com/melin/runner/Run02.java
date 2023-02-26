53aad0912f6239c95c20bff8c3326b3c27942ae9
DESKTOP-25G9A70\melin <melinhunter@gmail.com>
2023-02-26 22:24:48 +0800

    111 and 222

--- src/main/java/com/melin/runner/Run02.java
+++ src/main/java/com/melin/runner/Run02.java
@@ -6,9 +6,9 @@ import org.springframework.boot.SpringApplication;
 
 public class Run02 {
     public static void main(String[] args) {
-        System.out.println("test011");
+        System.out.println("test111");
         SpringApplication.run(SpringBootStandaloneApplication.class, args);
-        System.out.println("test022");
+        System.out.println("test122");
         SpringApplication.run(SpringBootStandalone2Application.class, args);
     }
 }
